2d48fcc1448945341a11e9c8a93dd033acbb5822
Perrine PULLICINO <Perrine PULLICINO>
2017-04-28 17:36:50 +0200

    Table Users

---
+++ src/UserBundle/Entity/user.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * user
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="UserBundle\Repository\userRepository")
+ */
+class user
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="stamina", type="smallint")
+     */
+    private $stamina;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return user
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return user
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set stamina
+     *
+     * @param integer $stamina
+     *
+     * @return user
+     */
+    public function setStamina($stamina)
+    {
+        $this->stamina = $stamina;
+
+        return $this;
+    }
+
+    /**
+     * Get stamina
+     *
+     * @return int
+     */
+    public function getStamina()
+    {
+        return $this->stamina;
+    }
+}
+
