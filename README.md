21f5eafbdcbaefb66bd98bdb51bf7c460a458ef6
Perrine PULLICINO <Perrine PULLICINO>
2017-04-28 15:37:17 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+StoryLine
+=========
+
+A Symfony project created on April 26, 2017, 12:25 pm.
---
+++ src/UserBundle/UserBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: perri
+ * Date: 26/04/2017
+ * Time: 17:54
+ */
+
+namespace UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UserBundle extends Bundle
+{
+}
