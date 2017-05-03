15891f8855ff7cf854dd0d5b7a408fc5ae5afd35
GaelleMarais <marais.gaelle@gmail.com>
2017-05-03 22:25:07 +0200

    Database

---
+++ src/UserBundle/Entity/CHARAC.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * CHARAC
+ *
+ * @ORM\Table(name="c_h_a_r_a_c")
+ * @ORM\Entity(repositoryClass="UserBundle\Repository\CHARACRepository")
+ */
+class CHARAC
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="user", type="integer")
+     */
+    private $user;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="level", type="integer")
+     */
+    private $level;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="life", type="integer")
+     */
+    private $life;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="attack", type="integer")
+     */
+    private $attack;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="race", type="string", length=255)
+     */
+    private $race;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="class", type="string", length=255)
+     */
+    private $class;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set user
+     *
+     * @param integer $user
+     *
+     * @return CHARAC
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return int
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return CHARAC
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set level
+     *
+     * @param integer $level
+     *
+     * @return CHARAC
+     */
+    public function setLevel($level)
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    /**
+     * Get level
+     *
+     * @return int
+     */
+    public function getLevel()
+    {
+        return $this->level;
+    }
+
+    /**
+     * Set life
+     *
+     * @param integer $life
+     *
+     * @return CHARAC
+     */
+    public function setLife($life)
+    {
+        $this->life = $life;
+
+        return $this;
+    }
+
+    /**
+     * Get life
+     *
+     * @return int
+     */
+    public function getLife()
+    {
+        return $this->life;
+    }
+
+    /**
+     * Set attack
+     *
+     * @param integer $attack
+     *
+     * @return CHARAC
+     */
+    public function setAttack($attack)
+    {
+        $this->attack = $attack;
+
+        return $this;
+    }
+
+    /**
+     * Get attack
+     *
+     * @return int
+     */
+    public function getAttack()
+    {
+        return $this->attack;
+    }
+
+    /**
+     * Set race
+     *
+     * @param string $race
+     *
+     * @return CHARAC
+     */
+    public function setRace($race)
+    {
+        $this->race = $race;
+
+        return $this;
+    }
+
+    /**
+     * Get race
+     *
+     * @return string
+     */
+    public function getRace()
+    {
+        return $this->race;
+    }
+
+    /**
+     * Set class
+     *
+     * @param string $class
+     *
+     * @return CHARAC
+     */
+    public function setClass($class)
+    {
+        $this->class = $class;
+
+        return $this;
+    }
+
+    /**
+     * Get class
+     *
+     * @return string
+     */
+    public function getClass()
+    {
+        return $this->class;
+    }
+}
+
---
+++ src/UserBundle/Entity/CLASS.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * CLASS
+ *
+ * @ORM\Table(name="c_l_a_s_s")
+ * @ORM\Entity(repositoryClass="UserBundle\Repository\CLASSRepository")
+ */
+class CLASS
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="competence", type="string", length=255)
+     */
+    private $competence;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return CLASS
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return CLASS
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set competence
+     *
+     * @param string $competence
+     *
+     * @return CLASS
+     */
+    public function setCompetence($competence)
+    {
+        $this->competence = $competence;
+
+        return $this;
+    }
+
+    /**
+     * Get competence
+     *
+     * @return string
+     */
+    public function getCompetence()
+    {
+        return $this->competence;
+    }
+}
+
