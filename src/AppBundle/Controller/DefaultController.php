615d6751f1affc99d687280f35b0ac185a4ae81d
Perrine PULLICINO <Perrine PULLICINO>
2017-05-05 01:22:32 +0200

    add titles to html

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,6 +13,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 class DefaultController extends Controller
 {
     public function giveHourlyStamina(){
+
         $em = $this->getDoctrine()->getManager();
 
         date_default_timezone_set('Europe/Paris');
@@ -83,88 +84,106 @@ class DefaultController extends Controller
      */
     public function createCharacterAction(Request $request){
 
-        $this->giveHourlyStamina();
-
         $usr= $this->get('security.token_storage')->getToken()->getUser();
 
-        $newcharac = new characters;
+        $characters = $this->getDoctrine()
+            ->getManager()
+            ->createQuery('SELECT e FROM GameBundle:characters e WHERE e.user='.($usr->getId()))
+            ->getResult();
 
-        $form = $this->createFormBuilder($newcharac)
-            ->add('name', TextType::class, array('attr' => array('autocomplete' => 'off', 'class' => 'form-control', 'style' => 'margin-bottom:15px')))
-            ->add('race', ChoiceType::class, array('choices' => array('Human' => 'Human', 'Troll' => 'Troll', 'Dwarf' => 'Dwarf', 'Elf' => 'Elf'), 'attr' => array('autocomplete' => 'off', 'class' => 'form-control', 'style' => 'margin-bottom:15px')))
-            ->add('class', ChoiceType::class, array('choices' => array('Thief' => 'Thief', 'Warrior' => 'Warrior', 'Mage' => 'Mage', 'Merchant' => 'Merchant'), 'attr' => array('autocomplete' => 'off', 'class' => 'form-control', 'style' => 'margin-bottom:15px')))
-            ->add('save', SubmitType::class, array('label' => 'Create', 'attr' => array('class' => 'btn btn-primary', 'style' => 'margin-bottom:15px')))
-            ->getForm();
 
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            //Get the data from the form
-            $name = $form['name']->getData();
-            $race = $form['race']->getData();
-            $class = $form['class']->getData();
-            //$now = new\DateTime('now');
-
-            $newcharac->setUser($usr);
-            $newcharac->setName($name);
-            $newcharac->setRace($race);
-            $newcharac->setClass($class);
-            $newcharac->setLevel(1);
-            $newcharac->setStory(10);
-
-            $attack = 0;
-            $life = 0;
-
-            if($race == 'Human') {
-                $attack = 30;
-                $life = 10;
-            } else if($race == 'Troll') {
-                $attack = 25;
-                $life = 15;
-            } else if($race == 'Dwarf') {
-                $attack = 32;
-                $life = 11;
-            } else if($race == 'Elf') {
-                $attack = 20;
-                $life = 12;
-            } else {
-
-            }
-
-            if($class == 'Thief') {
-                $life += 2;
-            } else if($class == 'Warrior') {
-                $attack += 5;
-            } else if($class == 'Magician') {
-                $life -= 3;
-            } else if($class == 'Merchant') {
-                $attack -= 3;
-            } else {
-
-            }
-
-            $newcharac->setLife($life);
-            $newcharac->setAttack($attack);
-
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($newcharac);
-
-            $em->flush();
-
+        if(count($characters) == 3) {
             $this->addFlash(
-                'notice',
-                'Character Created !'
+                'error',
+                'You already have 3 characters, you have to delete one first !'
             );
 
-            //CHANGE TO PAGE OF CHARAC LATER
             return $this->redirectToRoute('characterReview');
 
+        } else {
+
+            $this->giveHourlyStamina();
+
+            $usr= $this->get('security.token_storage')->getToken()->getUser();
+
+            $newcharac = new characters;
+
+            $form = $this->createFormBuilder($newcharac)
+                ->add('name', TextType::class, array('attr' => array('autocomplete' => 'off', 'class' => 'form-control', 'style' => 'margin-bottom:15px')))
+                ->add('race', ChoiceType::class, array('choices' => array('Human' => 'Human', 'Troll' => 'Troll', 'Dwarf' => 'Dwarf', 'Elf' => 'Elf'), 'attr' => array('autocomplete' => 'off', 'class' => 'form-control', 'style' => 'margin-bottom:15px')))
+                ->add('class', ChoiceType::class, array('choices' => array('Thief' => 'Thief', 'Warrior' => 'Warrior', 'Mage' => 'Mage', 'Merchant' => 'Merchant'), 'attr' => array('autocomplete' => 'off', 'class' => 'form-control', 'style' => 'margin-bottom:15px')))
+                ->add('save', SubmitType::class, array('label' => 'Create', 'attr' => array('class' => 'btn btn-primary', 'style' => 'margin-bottom:15px')))
+                ->getForm();
+
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                //Get the data from the form
+                $name = $form['name']->getData();
+                $race = $form['race']->getData();
+                $class = $form['class']->getData();
+                //$now = new\DateTime('now');
+
+                $newcharac->setUser($usr);
+                $newcharac->setName($name);
+                $newcharac->setRace($race);
+                $newcharac->setClass($class);
+                $newcharac->setLevel(1);
+                $newcharac->setStory(10);
+
+                $attack = 0;
+                $life = 0;
+
+                if($race == 'Human') {
+                    $attack = 30;
+                    $life = 10;
+                } else if($race == 'Troll') {
+                    $attack = 25;
+                    $life = 15;
+                } else if($race == 'Dwarf') {
+                    $attack = 32;
+                    $life = 11;
+                } else if($race == 'Elf') {
+                    $attack = 20;
+                    $life = 12;
+                } else {
+
+                }
+
+                if($class == 'Thief') {
+                    $life += 2;
+                } else if($class == 'Warrior') {
+                    $attack += 5;
+                } else if($class == 'Magician') {
+                    $life -= 3;
+                } else if($class == 'Merchant') {
+                    $attack -= 3;
+                } else {
+
+                }
+
+                $newcharac->setLife($life);
+                $newcharac->setAttack($attack);
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($newcharac);
+
+                $em->flush();
+
+                $this->addFlash(
+                    'notice',
+                    'Character Created !'
+                );
+
+                return $this->redirectToRoute('characterReview');
+
+            }
+
+            return $this->render('GameBundle:Characters:create.html.twig', array(
+                'form' => $form->createView()
+            ));
+
         }
-
-        return $this->render('GameBundle:Characters:create.html.twig', array(
-            'form' => $form->createView()
-        ));
-
     }
 
     /**
